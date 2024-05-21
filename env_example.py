cd02d06ce2b576944fb8e0162ef8d6f5eb7ba7d8
Jrets <24091974@mail.ru>
2024-05-21 22:34:16 +0300

    renew README

---
+++ env_example.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+# относительный путь, рекомендуется глобальный
+load_dotenv("./.env")
+
+key = "USERNAME1"
+username = os.getenv(key, None)
+username1 = os.environ.get(key, None)
+
+print(username)
+print(username1)
+
